8a236fd29aaa080059650ece45c3681e121d4b9a
Saikat Pradhan <saikatpradhan65@gmail.com>
2025-07-01 20:58:13 +0530

    'GoodNight'

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -11,6 +11,31 @@ app.get('/GreenNest', (req, res) => {
   res.sendFile(path.join(__dirname, '../FRONTEND/HTML/index.html'));
 });
 
+// Route for buyer-login page
+app.get('/buyer-login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../FRONTEND/HTML/buyer-login.html'));
+});
+
+// Route for seller-login page
+app.get('/seller-login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../FRONTEND/HTML/seller-login.html'));
+});
+
+// Route for forgot-password page
+app.get('/forgot-password.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../FRONTEND/HTML/forgot-password.html'));
+});
+
+// Route for otp-verification page
+app.get('/otp-verification.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../FRONTEND/HTML/otp-verification.html'));
+});
+
+// Route for reset-password page
+app.get('/reset-password.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../FRONTEND/HTML/reset-password.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
